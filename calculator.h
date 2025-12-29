3c19090061a87787e77fd7e1d19a641a9eb01b35
Sam1009-bit <tanyuenshen@gmail.com>
2025-12-29 10:13:29 +0800

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -1,11 +1,7 @@
-#include "calculator.h";
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
 
-  int add(int a, int b) 
-  {
-    return a + b;
-  }
+int add(int a, int b);
+int subtract(int a, int b);
 
-  int subtract(int a, int b)
-  {
-    return a - b;
-  }
+#endif
